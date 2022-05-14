b3226f518a682f352de5986a6acd18fdbded0cf2
oyako-li <oyakoli3@gmail.com>
2022-05-14 12:42:22 +0900

    upgrade scraper

--- src/search_words.py
+++ src/search_words.py
@@ -2,27 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import json
-# from tqdm import tqdm
+from tqdm import tqdm
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome import service as fs
 import time
 
-
 url = "https://thesaurus.weblio.jp/"
+path = "/User/matsuzawakazuki/Code/py_code/deep/"
 def setup_driver():
+    # global path
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    chrome_service = fs.Service(executable_path='/Users/matsuzawakazuki/Code/py_code/deep/chromedriver')
+    chrome_service = fs.Service(executable_path=f'./chromedriver')
     _driver = webdriver.Chrome(service=chrome_service, options=options)
 
     print('create chromeDriver')
     return _driver
 
 def searchRelatedWords(_word, _driver):
-    global ulr
+    # global ulr
     _results = []
     if _word == ' ':
         print('None')
@@ -49,9 +50,54 @@ def searchRelatedWords(_word, _driver):
     print(' '.join(_results))
     return _results, _driver
 
+def offload(_filename):
+    # global path
+    _path = f'./data/{_filename}.json'
+    jsonfile = open(_path)
+    return json.load(jsonfile)
+
+# 関連語を辞書オブジェクトに追加
+def related_words(_data):
+
+    _driver = setup_driver()
+    for key in tqdm(_data):
+        _labels = _data[key]['labels']
+        _results = []
+        _words = _labels.split(' ')
+        _words = list(dict.fromkeys(_words))
+        for word in _words:
+            while word:
+                try:
+                    _result, _driver = searchRelatedWords(word, _driver)
+                    _result = ' '.join(_result)
+                    _results.append(_result)
+                    break
+                except ValueError as e:
+                    print(e)
+                    _driver = setup_driver()
+                    print("change sleep mode")
+                    time.sleep(10)
+                    print("restart")
+
+        _labels = _words + _results
+        _labels = list(dict.fromkeys(_labels))
+        _labels = ' '.join(_labels)
+        _data[key]['labels'] = _labels
+    
+    return _data
+
+# jsonファイル化
+def upload(_filename, _submit_dict):
+  _jsonfile = f"./data/{_filename}.json"
+  _json_dic = _submit_dict
+  _jsonfile_open = open(_jsonfile,"w",encoding="utf-8")
+  json.dump(_json_dic , _jsonfile_open , ensure_ascii=False , indent=4)
+  _jsonfile_open.close()
+
 if __name__ == '__main__':
     driver = setup_driver()
-    print(searchRelatedWords("", driver))
+    print(searchRelatedWords("男性", driver))
+
 # soup = BeautifulSoup(r.text, 'html.parser')
 # t = soup.find_all('div')
 
